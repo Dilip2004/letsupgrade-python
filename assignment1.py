0e1ce865e8d6a19f119bb86a1f56daea0fd44368
Dilip2004 <65759261+Dilip2004@users.noreply.github.com>
2020-07-23 18:27:17 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+what is python? what are the ways to install python in your pc?
+a:Python is a general purpose and high level programming language. You can use Python for developing desktop GUI applications, websites and web applications. Also, Python, as a high level programming language, allows you to focus on core functionality of the application by taking care of common programming tasks.
+by using anaconda,pycharm,google colabs,repl.it
+what are code environments? types?
+a:types:1.text editors
+         2.full ide
+         3.notebook environment
